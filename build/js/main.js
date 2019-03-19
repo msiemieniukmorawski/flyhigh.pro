19e1e38ad976bf64fa6bd504bf5ff8621a4f9716
MS-M <msiemieniukmorawski@gmail.com>
2019-03-19 14:31:10 +0100

    change animation

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,22 @@
 "use strict";
 
+//open menu
 document.querySelector(".header__navigation").addEventListener("click", function () {
   document.querySelector(".menu__pimary").classList.toggle("open");
 });
-
+//close menu
 document.querySelector(".menu__close").addEventListener("click", function () {
   document.querySelector(".menu__pimary").classList.toggle("open");
 });
 
 (function () {
+  //animation box in who we are section
+  var controller = new ScrollMagic.Controller();
+  new ScrollMagic.Scene({
+    triggerElement: ".who"
+  }).setClassToggle(".who__box", "who__box--animation").addTo(controller);
+
+  //set position close menu
   var humburger = document.querySelector(".navigation__humbaurger");
   var close = document.querySelector(".menu__close");
   var humburgerPosition = humburger.getBoundingClientRect();
@@ -19,6 +27,7 @@ document.querySelector(".menu__close").addEventListener("click", function () {
     close.style.top = humburgerPosition.top + window.pageYOffset + 7 + "px";
     close.style.left = humburgerPosition.left + window.pageXOffset + "px";
   });
+  //init slider
   var siemaNav = document.querySelectorAll(".slider__nav__item");
   var mySiema = new Siema({
     selector: ".slider__wrapper",
@@ -37,15 +46,17 @@ document.querySelector(".menu__close").addEventListener("click", function () {
       siemaNav[this.currentSlide].classList.add("slider__nav__item--active");
     }
   });
+  //navigation slider
   for (var i = 0; i < siemaNav.length; i++) {
     siemaNav[i].addEventListener("click", function () {
-      for (var i = 0; i < siemaNav.length; i++) {
-        siemaNav[i].classList.remove("slider__nav__item--active");
+      for (var _i = 0; _i < siemaNav.length; _i++) {
+        siemaNav[_i].classList.remove("slider__nav__item--active");
       }
       this.classList.add("slider__nav__item--active");
       mySiema.goTo(this.dataset.slider - 1);
     });
   }
+  //autoplay slider
   setInterval(function () {
     return mySiema.next();
   }, 3000);
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,24 @@
+//open menu
 document
   .querySelector(".header__navigation")
   .addEventListener("click", function() {
     document.querySelector(".menu__pimary").classList.toggle("open");
   });
-
+//close menu
 document.querySelector(".menu__close").addEventListener("click", function() {
   document.querySelector(".menu__pimary").classList.toggle("open");
 });
 
 (() => {
+  //animation box in who we are section
+  const controller = new ScrollMagic.Controller();
+  new ScrollMagic.Scene({
+    triggerElement: ".who"
+  })
+    .setClassToggle(".who__box", "who__box--animation")
+    .addTo(controller);
+
+  //set position close menu
   const humburger = document.querySelector(".navigation__humbaurger");
   const close = document.querySelector(".menu__close");
   let humburgerPosition = humburger.getBoundingClientRect();
@@ -19,6 +29,7 @@ document.querySelector(".menu__close").addEventListener("click", function() {
     close.style.top = humburgerPosition.top + window.pageYOffset + 7 + "px";
     close.style.left = humburgerPosition.left + window.pageXOffset + "px";
   });
+  //init slider
   const siemaNav = document.querySelectorAll(".slider__nav__item");
   const mySiema = new Siema({
     selector: ".slider__wrapper",
@@ -31,20 +42,22 @@ document.querySelector(".menu__close").addEventListener("click", function() {
     threshold: 20,
     loop: true,
     onChange: function() {
-      for (var i = 0; i < siemaNav.length; i++) {
+      for (let i = 0; i < siemaNav.length; i++) {
         siemaNav[i].classList.remove("slider__nav__item--active");
       }
       siemaNav[this.currentSlide].classList.add("slider__nav__item--active");
     }
   });
-  for (var i = 0; i < siemaNav.length; i++) {
+  //navigation slider
+  for (let i = 0; i < siemaNav.length; i++) {
     siemaNav[i].addEventListener("click", function() {
-      for (var i = 0; i < siemaNav.length; i++) {
+      for (let i = 0; i < siemaNav.length; i++) {
         siemaNav[i].classList.remove("slider__nav__item--active");
       }
       this.classList.add("slider__nav__item--active");
       mySiema.goTo(this.dataset.slider - 1);
     });
   }
+  //autoplay slider
   setInterval(() => mySiema.next(), 3000);
 })();
