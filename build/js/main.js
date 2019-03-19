85abb6251b4053282cdf83c0a67ccdab7c5dd1a7
MS-M <msiemieniukmorawski@gmail.com>
2019-03-19 12:03:31 +0100

    finished section slider

--- build/js/main.js
+++ build/js/main.js
@@ -12,11 +12,41 @@ document.querySelector(".menu__close").addEventListener("click", function () {
   var humburger = document.querySelector(".navigation__humbaurger");
   var close = document.querySelector(".menu__close");
   var humburgerPosition = humburger.getBoundingClientRect();
-  close.style.top = humburgerPosition.top + window.scrollY + 7 + "px";
-  close.style.left = humburgerPosition.left + window.scrollX + "px";
-  window.addEventListener("resize", function (evt) {
+  close.style.top = humburgerPosition.top + window.pageYOffset + 7 + "px";
+  close.style.left = humburgerPosition.left + window.pageXOffset + "px";
+  window.addEventListener("resize", function () {
     var humburgerPosition = humburger.getBoundingClientRect();
-    close.style.top = humburgerPosition.top + window.scrollY + 7 + "px";
-    close.style.left = humburgerPosition.left + window.scrollX + "px";
+    close.style.top = humburgerPosition.top + window.pageYOffset + 7 + "px";
+    close.style.left = humburgerPosition.left + window.pageXOffset + "px";
   });
+  var siemaNav = document.querySelectorAll(".slider__nav__item");
+  var mySiema = new Siema({
+    selector: ".slider__wrapper",
+    duration: 200,
+    easing: "ease-out",
+    perPage: 1,
+    startIndex: 2,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: true,
+    onChange: function onChange() {
+      for (var i = 0; i < siemaNav.length; i++) {
+        siemaNav[i].classList.remove("slider__nav__item--active");
+      }
+      siemaNav[this.currentSlide].classList.add("slider__nav__item--active");
+    }
+  });
+  for (var i = 0; i < siemaNav.length; i++) {
+    siemaNav[i].addEventListener("click", function () {
+      for (var i = 0; i < siemaNav.length; i++) {
+        siemaNav[i].classList.remove("slider__nav__item--active");
+      }
+      this.classList.add("slider__nav__item--active");
+      mySiema.goTo(this.dataset.slider - 1);
+    });
+  }
+  setInterval(function () {
+    return mySiema.next();
+  }, 3000);
 })();
--- src/js/main.js
+++ src/js/main.js
@@ -12,11 +12,39 @@ document.querySelector(".menu__close").addEventListener("click", function() {
   const humburger = document.querySelector(".navigation__humbaurger");
   const close = document.querySelector(".menu__close");
   let humburgerPosition = humburger.getBoundingClientRect();
-  close.style.top = humburgerPosition.top + window.scrollY + 7 + "px";
-  close.style.left = humburgerPosition.left + window.scrollX + "px";
-  window.addEventListener("resize", evt => {
+  close.style.top = humburgerPosition.top + window.pageYOffset + 7 + "px";
+  close.style.left = humburgerPosition.left + window.pageXOffset + "px";
+  window.addEventListener("resize", () => {
     let humburgerPosition = humburger.getBoundingClientRect();
-    close.style.top = humburgerPosition.top + window.scrollY + 7 + "px";
-    close.style.left = humburgerPosition.left + window.scrollX + "px";
+    close.style.top = humburgerPosition.top + window.pageYOffset + 7 + "px";
+    close.style.left = humburgerPosition.left + window.pageXOffset + "px";
   });
+  const siemaNav = document.querySelectorAll(".slider__nav__item");
+  const mySiema = new Siema({
+    selector: ".slider__wrapper",
+    duration: 200,
+    easing: "ease-out",
+    perPage: 1,
+    startIndex: 2,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: true,
+    onChange: function() {
+      for (var i = 0; i < siemaNav.length; i++) {
+        siemaNav[i].classList.remove("slider__nav__item--active");
+      }
+      siemaNav[this.currentSlide].classList.add("slider__nav__item--active");
+    }
+  });
+  for (var i = 0; i < siemaNav.length; i++) {
+    siemaNav[i].addEventListener("click", function() {
+      for (var i = 0; i < siemaNav.length; i++) {
+        siemaNav[i].classList.remove("slider__nav__item--active");
+      }
+      this.classList.add("slider__nav__item--active");
+      mySiema.goTo(this.dataset.slider - 1);
+    });
+  }
+  setInterval(() => mySiema.next(), 3000);
 })();
